f5b8379813fc609cad3f28bdb9e9d3b2da9ddbc5
student <abhiewitec@gmail.com>
2021-02-27 16:32:04 +0000

    Add all files

---
+++ src/main/java/com/simplilearn/PipeSL/PipeSlApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.PipeSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeSlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipeSlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/PipeSL/PipeSlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.PipeSL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipeSlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
